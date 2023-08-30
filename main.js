74ea3725a89aa7b9d21aeedec6fddd1fd321bb3c
zaorsha <zaorsha@gmail.com>
2023-08-30 22:44:48 +0100

    Feat: create game() loop
    Bug: loop always resorts to player winning

--- main.js
+++ main.js
@@ -7,28 +7,40 @@ function playerSelection() {
      return prompt("Your move!");
 }
 
-let computerChoice = getComputerChoice();
-let playerChoice = playerSelection().toLowerCase();
-
+let computerChoice;
+let playerChoice;
+let computerScore = 0;
+let playerScore = 0;
 let result;
 
-switch (true) {
-    case computerChoice == playerChoice:
-        result = "Draw! Try again.";
-        break;
-    case computerChoice == "rock" || playerChoice == "paper":
-    case computerChoice == "paper" || playerChoice == "scissors":
-    case computerChoice == "scissors" || playerChoice == "rock":
-        result = "Player wins!";
-        break;
-    case computerChoice == "rock" || playerChoice == "scissors":
-    case computerChoice == "paper" || playerChoice == "rock":
-    case computerChoice == "scissors" || playerChoice == "paper":
-        result = "Computer wins!";
-        break;
-    default:
-        result = "Something's not right...";
+function game() {
+    for (let i = 0; i <= 4; i++) {
+        computerChoice = getComputerChoice();
+        playerChoice = playerSelection().toLowerCase();
+
+        switch (true) {
+            case computerChoice == playerChoice:
+                result = "Draw! Try again.";
+                break;
+            case computerChoice == "rock" || playerChoice == "paper":
+            case computerChoice == "paper" || playerChoice == "scissors":
+            case computerChoice == "scissors" || playerChoice == "rock":
+                result = "Player wins!";
+                playerScore += 1;
+                break;
+            case computerChoice == "rock" || playerChoice == "scissors":
+            case computerChoice == "paper" || playerChoice == "rock":
+            case computerChoice == "scissors" || playerChoice == "paper":
+                result = "Computer wins!";
+                computerScore += 1;
+                break;
+            default:
+                result = "Something's not right...";
+        }
+        console.log(`Computer chooses ${computerChoice}...`);
+        console.log(result);
+    }
 }
 
-console.log(`Computer chooses ${computerChoice}...`);
-console.log(result);
+game()
+console.log(`The final score is... Player: ${playerScore} | Computer: ${computerScore}`);
