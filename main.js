9ba9a9f015aea0f00fb8232969a2300a3cfe6051
zaorsha <zaorsha@gmail.com>
2023-08-30 22:24:20 +0100

    Feat: create if statement to choose winner

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let choice = ["Rock", "Paper", "Scissors"]
+    let choice = ["rock", "paper", "scissors"]
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
@@ -7,4 +7,28 @@ function playerSelection() {
      return prompt("Your move!");
 }
 
-console.log(getComputerChoice());
+let computerChoice = getComputerChoice();
+let playerChoice = playerSelection().toLowerCase();
+
+let result;
+
+if (computerChoice == playerChoice) {
+    result = "Draw! Try again";
+} else if (computerChoice == "rock" || playerChoice == "paper") {
+    result = "Player wins!";
+} else if (computerChoice == "rock" || playerChoice == "scissors") {
+    result = "Computer wins!";
+} else if (computerChoice == "paper" || playerChoice == "rock") {
+    result = "Computer wins!";
+} else if (computerChoice == "paper" || playerChoice == "scissors") {
+    result = "Player wins!";
+} else if (computerChoice == "scissors" || playerChoice == "rock") {
+    result = "Player wins!";
+} else if (computerChoice == "scissors" || playerChoice == "paper") {
+    result = "Computer wins!";
+} else {
+    result = "Something's not right...";
+}
+
+console.log(`Computer chooses ${computerChoice}...`);
+console.log(result);
