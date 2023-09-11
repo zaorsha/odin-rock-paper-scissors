9951d0d5bf8326406fca470a6c031c1b3391dfad
zaorsha <zaorsha@gmail.com>
2023-09-11 23:01:39 +0100

    Feat: buttons now work to select choice in console

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-const rockBtn = document.querySelector('.rock');
-const paperBtn = document.querySelector('.paper');
-const scissorsBtn = document.querySelector('.scissors');
+const buttons = document.querySelectorAll('.button');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
 function findWinner(pScore, cScore) {
     if (pScore > cScore) {
@@ -22,49 +23,51 @@ let playerChoice;
 // Function to return string for whichever
 // button was clicked
 
-function playerSelection() {
-    rockBtn.addEventListener('click', () => {
-        return 'rock';
-    });
-}
-
 let computerChoice;
 let computerScore = 0;
 let playerScore = 0;
 let result;
 
-function game() {
-
-
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice = button.innerText.toLowerCase();
         computerChoice = getComputerChoice();
-        playerChoice = playerSelection();
+        playRound(playerChoice, computerChoice);
+        game();
+    });
+})
 
-        switch (true) {
-            case computerChoice == playerChoice:
-                result = "Draw! Try again.";
-                break;
-            case computerChoice == "rock" && playerChoice == "paper":
-            case computerChoice == "paper" && playerChoice == "scissors":
-            case computerChoice == "scissors" && playerChoice == "rock":
-                result = "Player wins!";
-                playerScore += 1;
-                break;
-            case computerChoice == "rock" && playerChoice == "scissors":
-            case computerChoice == "paper" && playerChoice == "rock":
-            case computerChoice == "scissors" && playerChoice == "paper":
-                result = "Computer wins!";
-                computerScore += 1;
-                break;
-            default:
-                result = "Something's not right...";
-        }
+function playRound(playerChoice, computerChoice) {
+
+    switch (true) {
+        case computerChoice == playerChoice:
+            result = "Draw! Try again.";
+            break;
+        case computerChoice == "rock" && playerChoice == "paper":
+        case computerChoice == "paper" && playerChoice == "scissors":
+        case computerChoice == "scissors" && playerChoice == "rock":
+            result = "Player wins!";
+            playerScore += 1;
+            break;
+        case computerChoice == "rock" && playerChoice == "scissors":
+        case computerChoice == "paper" && playerChoice == "rock":
+        case computerChoice == "scissors" && playerChoice == "paper":
+            result = "Computer wins!";
+            computerScore += 1;
+            break;
+        default:
+            result = "Something's not right...";
+    }
+    return result;
+}
+
+function game() {        
 
         console.log(`Computer chooses ${computerChoice}...`);
         console.log(result);
     }
-}
 
 
-game();
+
 console.log(`The final score is... Player: ${playerScore} | Computer: ${computerScore}`);
 console.log(findWinner(playerScore,computerScore));
